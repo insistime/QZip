dce8230192a0e4191f4f2fc39c5b1efe1e7537ca
uikoo9 <uikoo9@qq.com>
2022-05-05 22:08:48 +0800

    feat: webpack splitChunks

--- qzip-src/qzip-renderer/webpack/qiao.webpack.js
+++ qzip-src/qzip-renderer/webpack/qiao.webpack.js
@@ -26,4 +26,21 @@ module.exports = {
   resolve   : {
     alias: alias,
   },
+  optimization: {
+    splitChunks: {
+      cacheGroups: {
+        react: {
+          test: /[\\/]node_modules[\\/](react|react-dom)[\\/]/,
+          name: 'react',
+          chunks: 'all',
+          priority: 0,
+          reuseExistingChunk: true,
+        },
+        default: {
+          priority: -20,
+          reuseExistingChunk: true,
+        },
+      },
+    },
+  },
 };
